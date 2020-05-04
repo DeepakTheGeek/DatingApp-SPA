9dc7528c0d980df01c0a227f051897753309495a
Deepak Das <Deepak.Das@brandmuscle.com>
2020-05-04 23:21:59 +0530

    Injecting angular service in component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   Login() {
-    console.log(this.model);
+    this.authService.login(this.model).subscribe(
+      (next) => {
+        console.log('logged in successfully.');
+      },
+      (error) => {
+        console.log('login failed');
+      }
+    );
   }
 }
