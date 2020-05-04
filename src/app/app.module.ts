2307afb7fc6cde8002617e2449dbb494b2acba6c
Deepak Das <Deepak.Das@brandmuscle.com>
2020-05-04 22:19:52 +0530

    Creating the Navigation and Login Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ValueComponent
+    ValueComponent,
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
