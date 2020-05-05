a0d73ced9c98cc52950be32749b95de51bbe088e
Deepak Das <Deepak.Das@brandmuscle.com>
2020-05-05 23:44:48 +0530

    Adding the register method to Auth Service

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,21 +1,27 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  @Input() valuesFromHome: any;
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   register() {
-    console.log(this.model);
+    this.authService.register(this.model).subscribe(
+      () => {
+        console.log('registration successful.');
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
   cancel() {
