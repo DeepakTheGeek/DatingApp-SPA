bbe63feabf25340d243e0e87f7ccae77c1891821
Deepak Das <Deepak.Das@brandmuscle.com>
2020-08-09 23:05:46 +0530

    validator feedback to users

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -12,18 +12,25 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
   registerForm: FormGroup;
+  username: AbstractControl;
+  password: AbstractControl;
+  confirmPassword: AbstractControl;
+
   constructor(private authService: AuthService, private alertify: AlertifyService) {}
 
   ngOnInit(): void {
     this.registerForm = new FormGroup({
       username: new FormControl('', Validators.required),
-      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(12)]),
       confirmPassword: new FormControl('', Validators.required)
     }, this.passwordMatchValidator);
+    this.username = this.registerForm.get('username');
+    this.password = this.registerForm.get('password');
+    this.confirmPassword = this.registerForm.get('confirmPassword');
   }
 
   passwordMatchValidator(g: FormGroup) {
-    return g.get('password').value === g.get('confirmPassword').value ? null : { mismatch: true };
+    return g.controls.password.value === g.controls.confirmPassword.value ? null : { mismatch: true };
   }
   register() {
     // this.authService.register(this.model).subscribe(
