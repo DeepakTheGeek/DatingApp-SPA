8583195572f6eeb97f5b9d2685d2f792595aacf4
Deepak Das <Deepak.Das@brandmuscle.com>
2020-08-11 23:02:55 +0530

    Completed registration implementation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -3,6 +3,8 @@ import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
 import { FormGroup, FormControl, Validators, AbstractControl, FormBuilder } from '@angular/forms';
 import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
+import { User } from '../_model/User';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -11,14 +13,18 @@ import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-  model: any = {};
+  user: User;
   registerForm: FormGroup;
   bsConfig: Partial<BsDatepickerConfig>;
   username: AbstractControl;
   password: AbstractControl;
   confirmPassword: AbstractControl;
 
-  constructor(private authService: AuthService, private alertify: AlertifyService, private fb: FormBuilder) {}
+  constructor(
+    private authService: AuthService,
+    private alertify: AlertifyService,
+    private fb: FormBuilder,
+    private router: Router) {}
 
   ngOnInit(): void {
     this.createRegisterForm();
@@ -47,15 +53,18 @@ export class RegisterComponent implements OnInit {
     return g.controls.password.value === g.controls.confirmPassword.value ? null : { mismatch: true };
   }
   register() {
-    // this.authService.register(this.model).subscribe(
-    //   () => {
-    //     this.alertify.success('registration successful.');
-    //   },
-    //   (error) => {
-    //     this.alertify.error(error);
-    //   }
-    // );
-    console.log(this.registerForm.value);
+    if (this.registerForm.valid) {
+      this.user = Object.assign({}, this.registerForm.value);
+      this.authService.register(this.user).subscribe(() => {
+        this.alertify.success('Registration successful.');
+      }, error => {
+        this.alertify.error(error);
+      }, () => {
+        this.authService.login(this.user).subscribe(() => {
+          this.router.navigate(['/members']);
+        });
+      });
+    }
   }
 
   cancel() {
