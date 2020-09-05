81ba4428ba6e147acfc7b8888acbc26a1a27d0f6
Deepak Das <Deepak.Das@brandmuscle.com>
2020-09-05 16:59:13 +0530

    Like Component Added

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -13,19 +13,27 @@ export class UserService {
   baseurl = environment.apiURL + 'users/';
   constructor(private http: HttpClient) {}
 
-  getUsers(page?: number, itemsPerPage?: number, userParams?: any): Observable<PaginatedResult<User[]>> {
+  getUsers(page?: number, itemsPerPage?: number, userParams?: any, likeParams?: string): Observable<PaginatedResult<User[]>> {
     const paginatedUsers = new PaginatedResult<User[]>();
+
     let params = new HttpParams();
-    if (page !== undefined && itemsPerPage !== undefined) {
+
+    if (page) {
       params = params.append('pageNumber', page.toString());
+    }
+    if (itemsPerPage) {
       params = params.append('pageSize', itemsPerPage.toString());
     }
-    if (userParams !== undefined) {
+    if (userParams) {
       params = params.append('gender', userParams.gender);
       params = params.append('minAge', userParams.minAge);
       params = params.append('maxAge', userParams.maxAge);
       params = params.append('orderBy', userParams.orderBy);
     }
+    if (likeParams) {
+      params = params.append(likeParams, 'true');
+    }
+
     return this.http
       .get<User[]>(this.baseurl, { observe: 'response', params})
       .pipe(
